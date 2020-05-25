66ea0950ba4518ce6295d6904bcd40ae45178ce0
dhslegen <dhslegen@163.com>
2020-05-25 14:52:55 +0800

    init

---
+++ src/main/java/com/dhslegen/leetcodepractice/problems/add_two_numbers/Main.java
@@ -0,0 +1,12 @@
+package com.dhslegen.leetcodepractice.problems.add_two_numbers;
+
+/**
+ * @author dhslegen
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/com/dhslegen/leetcodepractice/problems/two_sum/Main.java
@@ -0,0 +1,12 @@
+package com.dhslegen.leetcodepractice.problems.two_sum;
+
+/**
+ * @author dhslegen
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
