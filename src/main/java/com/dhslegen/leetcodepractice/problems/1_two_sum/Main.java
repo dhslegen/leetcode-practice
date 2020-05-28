bb111140d3d34dc569c8617260b374b49d083e47
dhslegen <dhslegen@163.com>
2020-05-28 23:21:59 +0800

    two-sum

---
+++ src/main/java/com/dhslegen/leetcodepractice/problems/1_two_sum/Main.java
@@ -0,0 +1,46 @@
+package com.dhslegen.leetcodepractice.problems.two_sum;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ * @author dhslegen
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 5};
+        int target = 11;
+        int[] ints = twoSum(nums, target);
+        System.out.println("结果为：" + Arrays.toString(ints));
+    }
+
+    /**
+     * 暴力法，遍历每个元素
+     *
+     * @param nums   整数数组
+     * @param target 目标值
+     * @return 和为目标值的数组的两个整数下标
+     */
+    private static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+}
