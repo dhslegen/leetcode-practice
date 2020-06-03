554208cdc2852aed4fce2f417b394cdbe9c488f7
dhslegen <dhslegen@163.com>
2020-06-03 10:31:21 +0800

    add_two_numbers：初始化题目

---
+++ src/main/java/com/dhslegen/leetcodepractice/problems/no2/add_two_numbers/Main.java
@@ -0,0 +1,31 @@
+package com.dhslegen.leetcodepractice.problems.no2.add_two_numbers;
+
+/**
+ * <h2>两数相加
+ * <p>
+ * 给出两个 <b>非空</b> 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 <b>逆序</b> 的方式存储的，并且它们的每个节点只能存储 <b>一位</b> 数字。
+ * <p>
+ * <p>
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * <p>
+ * <p>
+ * 您可以假设除了数字 <b>0</b> 之外，这两个数都不会以 <b>0</b> 开头。
+ * <p>
+ * <h3>示例：
+ * <p>
+ * <b>输入：</b>(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * <p>
+ * <b>输出：</b>7 -> 0 -> 8
+ * <p>
+ * <b>原因：</b>342 + 465 = 807
+ * <p>
+ *
+ * @author dhslegen
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
