805a8438bbab2ded1935fabad0fd2643769bf530
zhaowenhao <dhslegen@163.com>
2020-07-16 16:33:46 +0800

    add_two_numbers：增加描述

---
+++ src/main/java/com/dhslegen/leetcodepractice/problems/no3/longest_substring_without_repeating_characters/Main.java
@@ -0,0 +1,26 @@
+package com.dhslegen.leetcodepractice.problems.no3.longest_substring_without_repeating_characters;
+
+/**
+ * <h2>无重复字符的最长子串
+ * <p>
+ * 给定一个字符串，请你找出其中不含有重复字符的 <b>最长子串</b> 的长度。
+ * <p>
+ * <h3>示例 1:
+ * <p>
+ * <b>输入:</b> "abcabcbb"
+ * <b>输出:</b> 3
+ * <b>解释:</b> 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * <h3>示例 2:
+ * <p>
+ * <b>输入:</b> "bbbbb"
+ * <b>输出:</b> 1
+ * <b>解释:</b> 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * <h3>示例 3:
+ * <p>
+ * <b>输入:</b> "pwwkew"
+ * <b>输出:</b> 3
+ * <b>解释:</b> 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 <b>子串</b> 的长度，"pwke" 是一个子序列，不是子串。
+ */
+public class Main {
+}
